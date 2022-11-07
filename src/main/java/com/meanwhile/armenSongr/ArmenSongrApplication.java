fbbe4e08fa260715fede5fa4ad7693d5d1994fff
Lolunts <armasconi@gmail.com>
2022-11-07 12:48:20 -0800

    start

---
+++ src/main/java/com/meanwhile/armenSongr/ArmenSongrApplication.java
@@ -0,0 +1,13 @@
+package com.meanwhile.armenSongr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmenSongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmenSongrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meanwhile/armenSongr/ArmenSongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meanwhile.armenSongr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmenSongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
