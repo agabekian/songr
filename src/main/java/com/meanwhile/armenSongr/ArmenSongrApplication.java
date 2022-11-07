bd61d4eb858b6b1398400490330b449b36455f16
Lolunts <armasconi@gmail.com>
2022-11-07 13:22:46 -0800

    hellow

--- src/main/java/com/meanwhile/armenSongr/ArmenSongrApplication.java
+++ src/main/java/com/meanwhile/armenSongr/ArmenSongrApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArmenSongrApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ArmenSongrApplication.class, args);
 	}
-
 }
